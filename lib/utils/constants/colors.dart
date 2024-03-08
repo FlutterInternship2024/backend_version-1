89b7c4771816794fb0923ea35f4b2eaa08f4c7ed
Your Name <you@example.com>
2024-03-08 23:19:58 +0530

    SignUp done and File structure

--- lib/utils/constants/colors.dart
+++ lib/utils/constants/colors.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 
 class TColors {
   // App theme colors
-  static const Color primary = Color(0xFF4b68ff);
-  static const Color secondary = Color(0xFFFFE24B);
-  static const Color accent = Color(0xFFb0c7ff);
+  static const Color primary = Colors.green;
+  static const Color secondary = Colors.greenAccent;
+  static const Color accent = Colors.white;
 
   // Text colors
   static const Color textPrimary = Color(0xFF333333);
@@ -12,16 +12,16 @@ class TColors {
   static const Color textWhite = Colors.white;
 
   // Background colors
-  static const Color light = Color(0xFFF6F6F6);
-  static const Color dark = Color(0xFF272727);
-  static const Color primaryBackground = Color(0xFFF3F5FF);
+  static const Color light = Colors.white;
+  static const Color dark = Colors.black;
+  static const Color primaryBackground = Colors.green;
 
   // Background Container colors
   static const Color lightContainer = Color(0xFFF6F6F6);
   static Color darkContainer = TColors.white.withOpacity(0.1);
 
   // Button colors
-  static const Color buttonPrimary = Color(0xFF4b68ff);
+  static const Color buttonPrimary = Colors.green;
   static const Color buttonSecondary = Color(0xFF6C757D);
   static const Color buttonDisabled = Color(0xFFC4C4C4);
 
@@ -43,8 +43,4 @@ class TColors {
   static const Color softGrey = Color(0xFFF4F4F4);
   static const Color lightGrey = Color(0xFFF9F9F9);
   static const Color white = Color(0xFFFFFFFF);
-
 }
-
-
-
